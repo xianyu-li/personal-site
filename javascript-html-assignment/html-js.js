8678e4648a07b0ffe2bb1be29ac3d4b112f5ce8e
Xianyu Li <lix113@newschool.edu>
2020-02-28 13:49:05 -0500

    added javascript html assignment

---
+++ javascript-html-assignment/html-js.js
@@ -0,0 +1,66 @@
+function changeHeading() {
+    document.get
+}
+function changeText() {
+    document.getElementById('heading').style.textAlign="right";
+}
+
+function normalText() {
+    document.getElementById('heading').style.fontFamily="Georgia,"
+    document.getElementById('heading').style.fontWeight="Bold"
+    document.getElementById('heading').style.textAlign="center";
+    document.getElementById('heading').style.fontSize="50px";
+
+}
+
+function changeHeadingTwo() {
+    document.getElementById('heading-two').style.marginLeft="50px";
+    document.getElementById('heading-two').style.textTransform="uppercase";
+    document.getElementById('heading-two').style.letterSpacing="2px";
+}
+
+
+
+function changeM() {
+    document.getElementById('p1').style.marginLeft="60px";
+    document.getElementById('p1').style.marginRight="60px";
+    document.getElementById('p1').style.fontFamily="Georgia"; 
+    document.getElementById('p2').style.marginLeft="60px";
+    document.getElementById('p2').style.marginRight="60px";
+    document.getElementById('p2').style.fontFamily="Georgia";
+    document.getElementById('p3').style.marginLeft="60px";
+    document.getElementById('p3').style.marginRight="60px";
+    document.getElementById('p3').style.fontFamily="Georgia";
+    document.getElementById('p4').style.marginLeft="60px";
+    document.getElementById('p4').style.marginRight="60px";
+    document.getElementById('p4').style.fontFamily="Georgia";
+}
+
+function changeQ() {
+    document.getElementById('quote').style.fontFamily="Georiga";
+    document.getElementById('quote').style.fontSize="30px";
+    document.getElementById('quote').style.marginLeft="20%";
+    document.getElementById('quote').style.marginRight="40%";
+
+
+}
+
+
+
+
+
+
+
+
+
+
+function darkMode() {
+    document.getElementById('everything').style.backgroundColor="black";
+    document.getElementById('everything').style.color="lightgray";
+}
+
+function reset() {
+    document.getElementById('everything').style.backgroundColor="white"
+    document.getElementById('everything').style.color="black"
+}
+

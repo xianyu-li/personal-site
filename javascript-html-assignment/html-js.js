1ff00ca4737b55a3d85c6e839b013115cfdd72c5
Xianyu Li <lix113@newschool.edu>
2020-02-28 16:09:05 -0500

    more updates

--- javascript-html-assignment/html-js.js
+++ javascript-html-assignment/html-js.js
@@ -1,6 +1,4 @@
-function changeHeading() {
-    document.get
-}
+
 function changeText() {
     document.getElementById('heading').style.textAlign="right";
 }
@@ -13,38 +11,59 @@ function normalText() {
 
 }
 
+function altHeading() {
+    document.getElementById('heading').style.fontFamily="courier";
+    document.getElementById('heading').innerHTML="01011010 01100101 01110010 01101111 01100101 01110011 00100000 01100001 01101110 01100100 00100000 01001111 01101110 01100101 01110011 00101100 00100000 01010011 01100001 01100100 01101001 01100101 00100000 01010000 01101100 01100001 01101110 01110100 00001010";
+}
+
+function altHeadingTwo() {
+    document.getElementById('heading-two').style.letterSpacing="6px";
+}
 function changeHeadingTwo() {
     document.getElementById('heading-two').style.marginLeft="50px";
     document.getElementById('heading-two').style.textTransform="uppercase";
     document.getElementById('heading-two').style.letterSpacing="2px";
 }
 
+function altParagraph() {
+    document.getElementById('p1').innerHTML="˙ǝɯoɔ oʇ sǝɔᴉɹʇɐɯ puɐ 'ʇǝN ǝɥʇ 'qǝM ǝpᴉM plɹoM ǝɥʇ 'sǝuᴉl ɯoɔǝlǝʇ 'suǝǝɹɔs pǝlǝxᴉd 'sǝlqɐɔ ɔᴉʇdo-ɹǝqᴉɟ 'spuɐɹʇs uoɔᴉlᴉs 'sʇuǝɯɐlᴉɟ lɐɔᴉɹʇɔǝlǝ 'sɹǝqᴉɟ ɔᴉʇǝɥʇuʎs 'sǝʇᴉʍ ǝuoɥdǝlǝʇ 'sǝƃɐᴉɹɹɐɔ 'sɹǝʇᴉɹʍǝdʎʇ 'suǝd puɐ sǝɥsnɹq 'ɹǝdɐd puɐ sɐʌuɐɔ 'ʞlᴉs puɐ uoʇʇoɔ 'sɯool puɐ sǝlʇʇnɥs 'sɔᴉɹqɐɟ ɟo uoᴉʇɐɔᴉɹqɐɟ ǝɥʇ ɥƃnoɹɥʇ ɥʇɹoɟ puɐ ʞɔɐq 'ƃuᴉʌɐǝʍ puɐ ƃuᴉuuᴉds ɟo sǝƃɐ ǝɥʇ ɥƃnoɹɥʇ uʍop puɐ dn 'sɯool pǝʇɐɯoʇnɐ ɟo sǝloɥ pǝɥɔund ǝɥʇ ɟo ʇno puɐ uI ˙sʇɹɐ puɐ sǝɔuǝᴉɔs ǝɥʇ 'ʎƃolouɥɔǝʇ 'ƃuᴉʇndɯoɔ ɟo ʎɹoʇsᴉɥ ǝɥʇ ɥƃnoɹɥʇ uɹnʇ puɐ ʇsᴉʍʇ ɥɔᴉɥʍ spɐǝɹɥʇ ɟo ƃuᴉɹǝɥʇɐƃ ɐ 'lɐᴉɹ­ǝʇɐɯ ʎldɯᴉs ǝʇᴉnb ʇnq 'lɐɹǝʇᴉl ɹou lɐɔᴉɹoɥdɐʇǝɯ ɹǝɥʇᴉǝu sᴉ uɹɐʎ ǝɥ┴ ,,˙sʞlᴉs pǝɹnƃᴉɟ ǝʇɐɹoqɐlǝ ɟo ƃuᴉʌɐǝʍ ǝɥʇ oʇ ɹǝɥʇɐɹ pǝʇɐlǝɹ ʎlʇɔǝɹᴉp sᴉ ʇᴉ ;ƃuᴉʇᴉɹʍ ɥʇᴉʍ uᴉƃǝq ʇou sǝop,, ɥɔᴉɥʍ ǝuoz ǝʌᴉsɹǝɯ­ɯᴉ uɐ ɟo sʇuǝuodɯoɔ snoʇᴉɔᴉldᴉʇlnɯ ǝɥʇ 'ǝʌᴉʇɔɐɹǝdʎɥ puɐ ǝʌᴉʇ­ɔɐɹǝʇuᴉ ǝɯoɔǝq ɐᴉpǝW ˙pǝɯɐuun ʇǝʎ sɐ slǝuuɐɥɔ ssǝlʇunoɔ puɐ 'sǝɹnʇɔǝʇᴉɥɔɹɐ 'sǝɹnʇxǝʇ ǝlᴉʇɔɐʇ 'sllǝɯs 'sǝdɐɥs 'ɔᴉsnɯ 'sɹǝqɯnu 'spɹoʍ pǝʇɐlosᴉ ǝɔuo ǝɹǝʍ ʇɐɥʍ ɯoɹɟ sʞɹoʍʇǝu ʍǝu ǝʌɐǝʍ ʎɹnʇ­uǝɔ ɥʇǝᴉʇuǝʍʇ ǝʇɐl ǝɥʇ ɟo sǝuᴉɥɔɐɯ lɐʇᴉƃᴉp ǝɥʇ os 'sqǝʍ pǝlƃuɐʇ ʎlǝʇᴉuᴉɟuᴉ ɟo sʇuǝɯɐlᴉɟ ǝɯoɔǝq ǝʌɐɥ sʇxǝʇ pǝʇɐnpᴉʌᴉpuᴉ sɐ ʇsnſ"
+}
 
+function changeBackward() {
+    document.getElementById('p2').innerHTML=".eb ot desoppus won saw taht revetahW .flesti ytilaer htiw suounitnoc seitilaer latigid evisremmi ,snoit­cennoc laruen dna smsinahcem kcabdeef yrosnes ,stius atad dna stemleh ytilaer lautriv htiw egrevnoc ot denitsed demees won hcihw aidemitlum evitcaretni eht otni desuf stxet dna ,seciov ,sdnuos ,segami llits ,oediV .teN eht dellac krowten labolg tsav eht eht fo sedon emoceb ot srossecorp drow dna srotaluclac detalosi sa ecnacifingis rieht tsol sretupmoc ,edaced txen eht fo esruoc eht nI .egrevnoc ot stnenopmoc eseht dewolla hcihw wasgij eht fo eceip rehtona osla dna ,noitcif a saw recnamorueN .evisremmi ylgnisaercni dna evitcidda erew semag edacra dna ,detacitsihpos ylhgih eb ot nwonk erew skrowten snoitacinummocelet dna seigolonhcet noitalumis yratilim ,senohpelet sa suotiuqibu sa gnimoceb erew sretupmoc lanosreP .laer ylgnisaercni flesti saw hcihw ytilaer lautriv a saw tI .ysatnaf dna htym fo sria niht eht fo tuo dekculp enoz a ron ,enalp gnitsixe yllautca na rehtien saw debircsed ti ecapsrebyc eht ,4891 ni dehsilbup saw recnamorueN s'nosbiG mailliW ,slevon knuprebyc eht fo tsrif eht nehW"
+}
 
+function upsidedownText() {
+    
+}
 function changeM() {
     document.getElementById('p1').style.marginLeft="60px";
-    document.getElementById('p1').style.marginRight="60px";
+    document.getElementById('p1').style.marginRight="100px";
     document.getElementById('p1').style.fontFamily="Georgia"; 
     document.getElementById('p2').style.marginLeft="60px";
-    document.getElementById('p2').style.marginRight="60px";
+    document.getElementById('p2').style.marginRight="100px";
     document.getElementById('p2').style.fontFamily="Georgia";
     document.getElementById('p3').style.marginLeft="60px";
-    document.getElementById('p3').style.marginRight="60px";
+    document.getElementById('p3').style.marginRight="100px";
     document.getElementById('p3').style.fontFamily="Georgia";
     document.getElementById('p4').style.marginLeft="60px";
-    document.getElementById('p4').style.marginRight="60px";
+    document.getElementById('p4').style.marginRight="100px";
     document.getElementById('p4').style.fontFamily="Georgia";
 }
 
 function changeQ() {
     document.getElementById('quote').style.fontFamily="Georiga";
     document.getElementById('quote').style.fontSize="30px";
-    document.getElementById('quote').style.marginLeft="20%";
-    document.getElementById('quote').style.marginRight="40%";
-
+    document.getElementById('quote').style.marginLeft="40%";
+    document.getElementById('quote').style.marginRight="50%";
+    document.getElementById('quote').style.backgroundColor="pink";  
 
 }
 
+function changeQuote() {
+    document.getElementById('quote').style.marginLeft="30px";
+    document.getElementById('quote').style.marginRight="30px";
+}
 
 
 
@@ -60,7 +79,7 @@ function darkMode() {
 }
 
 function reset() {
-    document.getElementById('everything').style.backgroundColor="white"
-    document.getElementById('everything').style.color="black"
+    document.getElementById('everything').style.backgroundColor="pink"
+    document.getElementById('everything').style.color="teal "
 }
 

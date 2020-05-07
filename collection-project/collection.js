fa4af9af472b48691259d97df91e825da2cb4f19
Xianyu Li <lix113@newschool.edu>
2020-05-06 20:42:00 -0400

    yea

--- collection-project/collection.js
+++ collection-project/collection.js
@@ -61,58 +61,6 @@ var emojis  = [
 
 ]
 
-// var emojis = [
-// {
-//     "date": "14",
-//     "day": "Tuesday",
-//     "emotions" : ["<div class='blush'>&#128522;</div>","<div class='blush'>&#128522;</div>","<div class='blush'>&#128522;</div>","<div class='blush'>&#128522;</div>","<div class='blush'>&#128522;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='unsure'>&#128533;</div>","<div class='unsure'>&#128533;</div>","<div class='unsure'>&#128533;</div>","<div class='unsure'>&#128533;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='meh'>&#128528;</div>","<div class='sorry'>&#128532;</div>","<div class='sorry'>&#128532;</div>","<div class='sorry'>&#128532;</div>","<div class='meh'>&#128528;</div>","<div class='meh'>&#128528;</div>","<div class='meh'>&#128528;</div>","<div class='blush'>&#128522;</div>","<div class='blush'>&#128522;</div>"]
-// },
-// {
-//     "date": "15",
-//     "day": "Wednesday",
-//     "emotions" : ["<div class='blush'>&#128522;</div>","<div class='blush'>&#128522;</div>","<div class='blush'>&#128522;</div>","<div class='blush'>&#128522;</div>","<div class='blush'>&#128522;</div>","<div class='blush'>&#128522;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='meh'>&#128528;</div>","<div class='meh'>&#128528;</div>","<div class='meh'>&#128528;</div>","<div class='meh'>&#128528;</div>","<div class='sorry'>&#128532;</div>","<div class='sorry'>&#128532;</div>","<div class='sweat'>&#128560;</div>","<div class='sweat'>&#128560;</div>","<div class='meh'>&#128528;</div>","<div class='meh'>&#128528;</div>"]
-// },
-// {
-//     "date": "16",
-//     "day": "Thursday",
-//     "emotions": ["<div class='meh'>&#128528;</div>","<div class='meh'>&#128528;</div>","<div class='meh'>&#128528;</div>","<div class='blush'>&#128522;</div>","<div class='blush'>&#128522;</div>","<div class='blush'>&#128522;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='blush'>&#128522;</div>","<div class='blush'>&#128522;</div>","<div class='unsure'>&#128533;</div>","<div class='unsure'>&#128533;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='sweat'>&#128560;</div>","<div class='sweat'>&#128560;</div>","<div class='pissed'>&#128544;</div>","<div class='pissed'>&#128544;</div>","<div class='sweat'>&#128560;</div>","<div class='sweat'>&#128560;</div>","<div class='sorry'>&#128532;</div>","<div class='sorry'>&#128532;</div>","<div class='blush'>&#128522;</div>"]
-// },
-// {
-//     "date": "17",
-//     "day": "Friday",
-//     "emotions": ["<div class='blush'>&#128522;</div>","<div class='blush'>&#128522;</div>","<div class='blush'>&#128522;</div>",,"<div class='blush'>&#128522;</div>","<div class='blush'>&#128522;</div>","<div class='blush'>&#128522;</div>","<div class='blush'>&#128522;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='meh'>&#128528;</div>","<div class='meh'>&#128528;</div>","<div class='sweat'>&#128560;</div>","<div class='unsure'>&#128533;</div>","<div class='unsure'>&#128533;</div>","<div class='pissed'>&#128544;</div>","<div class='sorry'>&#128532;</div>","<div class='sorry'>&#128532;</div>","<div class='meh'>&#128528;</div>"]
-// },
-// {
-//     "date": "18",
-//     "day": "Saturday",
-//     "emotions": ["<div class='meh'>&#128528;</div>","<div class='meh'>&#128528;</div>","<div class='blush'>&#128522;</div>","<div class='blush'>&#128522;</div>","<div class='blush'>&#128522;</div>","<div class='blush'>&#128522;</div>","<div class='blush'>&#128522;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='meh'>&#128528;</div>","<div class='meh'>&#128528;</div>","<div class='blush'>&#128522;</div>","<div class='sorry'>&#128532;</div>","<div class='sorry'>&#128532;</div>","<div class='sorry'>&#128532;</div>","<div class='meh'>&#128528;</div>","<div class='meh'>&#128528;</div>","<div class='meh'>&#128528;</div>"]
-// },
-// {
-//     "date": "19",
-//     "day": "Sunday",
-//     "emotions": ["<div class='meh'>&#128528;</div>","<div class='meh'>&#128528;</div>","<div class='meh'>&#128528;</div>","<div class='meh'>&#128528;</div>","<div class='meh'>&#128528;</div>","<div class='blush'>&#128522;</div>","<div class='blush'>&#128522;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='sweat'>&#128560;</div>","<div class='sweat'>&#128560;</div>","<div class='sweat'>&#128560;</div>","<div class='meh'>&#128528;</div>","<div class='meh'>&#128528;</div>","<div class='meh'>&#128528;</div>","<div class='meh'>&#128528;</div>","<div class='sorry'>&#128532;</div>","<div class='sorry'>&#128532;</div>"]
-// },
-// {
-//     "date": "20",
-//     "day": "Monday",
-//     "emotions": ["<div class='sorry'>&#128532;</div>","<div class='sorry'>&#128532;</div>","<div class='sorry'>&#128532;</div>","<div class='sweat'>&#128560;</div>","<div class='sweat'>&#128560;</div>","<div class='sweat'>&#128560;</div>","<div class='sweat'>&#128560;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='meh'>&#128528;</div>","<div class='meh'>&#128528;</div>","<div class='meh'>&#128528;</div>","<div class='sweat'>&#128560;</div>","<div class='sweat'>&#128560;</div>","<div class='sweat'>&#128560;</div>","<div class='unsure'>&#128533;</div>","<div class='unsure'>&#128533;</div>","<div class='unsure'>&#128533;</div>","<div class='blush'>&#128522;</div>"]
-// },
-// {
-//     "date": "21",
-//     "day": "Tuesday",
-//     "emotions": ["<div class='blush'>&#128522;</div>","<div class='blush'>&#128522;</div>","<div class='blush'>&#128522;</div>","<div class='blush'>&#128522;</div>","<div class='blush'>&#128522;</div>","<div class='blush'>&#128522;</div>","<div class='blush'>&#128522;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='sweat'>&#128560;</div>","<div class='sorry'>&#128532;</div>","<div class='sorry'>&#128532;</div>","<div class='meh'>&#128528;</div>","<div class='meh'>&#128528;</div>","<div class='meh'>&#128528;</div>","<div class='meh'>&#128528;</div>","<div class='meh'>&#128528;</div>","<div class='meh'>&#128528;</div>"]
-// },
-// {
-//     "date": "22",
-//     "day": "Wednesday",
-//     "emotions": ["<div class='meh'>&#128528;</div>","<div class='meh'>&#128528;</div>","<div class='meh'>&#128528;</div>","<div class='meh'>&#128528;</div>","<div class='blush'>&#128522;</div>","<div class='blush'>&#128522;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='sorry'>&#128532;</div>","<div class='sorry'>&#128532;</div>","<div class='sweat'>&#128560;</div>","<div class='sweat'>&#128560;</div>","<div class='unsure'>&#128533;</div>","<div class='unsure'>&#128533;</div>","<div class='unsure'>&#128533;</div>","<div class='unsure'>&#128533;</div>","<div class='unsure'>&#128533;</div>","<div class='unsure'>&#128533;</div>"]
-// },
-// {
-//     "date": "23",
-//     "day": "Thursday",
-//     "emotions" : ["<div class='unsure'>&#128533;</div>","<div class='unsure'>&#128533;</div>","<div class='blush'>&#128522;</div>","<div class='blush'>&#128522;</div>","<div class='blush'>&#128522;</div>","<div class='unsure'>&#128533;</div>","<div class='unsure'>&#128533;</div>","<div class='unsure'>&#128533;</div>","<div class='unsure'>&#128533;</div>","<div class='unsure'>&#128533;</div>","<div class='unsure'>&#128533;</div>","<div class='unsure'>&#128533;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='zzz'>&#128564;</div>","<div class='sweat'>&#128560;</div>","<div class='sweat'>&#128560;</div>","<div class='meh'>&#128528;</div>","<div class='meh'>&#128528;</div>","<div class='meh'>&#128528;</div>","<div class='unsure'>&#128533;</div>","<div class='blush'>&#128522;</div>","<div class='blush'>&#128522;</div>"]
-// }]
-
 console.log(emojis);
 console.log(emojis[0].emotions);
 for(var i = 0; i < emojis.length; i++ ) {
@@ -121,9 +69,4 @@ for(var i = 0; i < emojis.length; i++ ) {
         $('.row').append("<div>" + emojis[i].emotions[j] + "</div>")
     }
 }
-
-// $(emojis.emotions).hover(function() {
-//     $(emojis[0].emotions)
-// }
-
 
